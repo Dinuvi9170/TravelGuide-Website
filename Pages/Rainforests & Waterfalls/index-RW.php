b33ec0a74778974308a983d68954a831678b4110
Dinuvi Tharushika <135813670+Dinuvi9170@users.noreply.github.com>
2025-09-05 23:21:20 +0530

    Add files via upload

---
+++ Pages/Rainforests & Waterfalls/index-RW.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rain forests & Waterfalls-walkers</title>
+     <link rel="icon" href="images/walkers-footer.png" />
+    <!--jQuery-->
+    <script
+    src="https://code.jquery.com/jquery-3.7.0.min.js"
+    integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g="
+    crossorigin="anonymous"
+  ></script>
+
+  <!--Bootstrap-->
+  <link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+    rel="stylesheet"
+    integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+    crossorigin="anonymous"
+  />
+  <script
+    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+    crossorigin="anonymous"
+  ></script>
+
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
+
+  <link rel="stylesheet" href="css/style-RW.css"/>
+</head>
+<body>
+  <!--NavBar--------------------------------------------Start-->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-light fixed-top navbar-scrolled">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"
+        ><img src="images/walkers1.png " style="width: 40px; height: auto" alt=""
+      /></a>
+      <h5 style="font-weight: 800"></h5>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto me-2">
+          <li class="nav-item">
+            <a
+              class="nav-link btn btn-outline-dark"
+              aria-current="page"
+              href="#"
+              >Home</a>
+          </li>
+          <li class="nav-item">
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+              <ul class="navbar-nav">
+                <li class="nav-item dropdown">
+                  <a class="nav-link active dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Catogaries
+                  </a>
+                  <ul class="dropdown-menu dropdown-menu-light" aria-labelledby="navbarDarkDropdownMenuLink">
+                    <li><a class="dropdown-item" href="#">Wild-Life & Safari</a></li>
+                    <li><a class="dropdown-item active-color" href="#">Rainforests & Waterfalls</a></li>
+                    <li><a class="dropdown-item" href="#">Beaches & Activities</a></li>
+                    <li><a class="dropdown-item" href="#">Heritage & Religious</a></li>
+                    <li><a class="dropdown-item" href="#">Hiking, Camping & Adventure</a></li>
+                    <li><a class="dropdown-item" href="#">Accomedations</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-outline-dark" href="#">FAQ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-outline-dark" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-outline-dark" href="#">Join Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <!--NavBar--------------------------------------------------------End-->
+
+<!--Hero Sec------------------------------------------------------Start-->
+
+<div class="herosection">
+  <div class="herotopic">
+  <h1>Rain forests & Waterfalls Sri Lanka</h1>
+</div>
+</div>
+
+<!--Hero Sec------------------------------------------------------End-->
+
+
+<div class="container mt-5 mb-5">
+  <div class="row">
+    <div class="col-lg-4 col-md-6 col-sm-12" style="background-color: #D3D0D0; padding: 0;">
+      <br> &nbsp;<center>
+      <h4><b><font style="font-family: Poppins, 'Lucida Bright', 'DejaVu Serif', Georgia, 'serif';" color="black">Rainforests in Sri Lanka</font></b></h4></center><br>
+      <p style="margin: 20px;"> <font style="font-size: 15px; ">
+        Sri Lanka's rainforests are a natural wonder, teeming with rich biodiversity and enchanting landscapes. Among them, the Sinharaja Forest Reserve and the Knuckles Mountain Range stand out as prime examples of the island's ecological diversity. Lush canopies, vibrant flora, and rare fauna make these rainforests a paradise for nature enthusiasts and researchers alike. The symphony of endemic birds, elusive leopards, and endemic plant species adds to the allure of these enchanting ecosystems. Visitors can immerse themselves in the serene ambiance, hike through ancient trails, and discover hidden waterfalls. Sri Lanka's rainforests are not just scenic wonders; they also play a crucial role in preserving the island's ecological balance.</font></p>
+    </div>
+    <div class="col-lg-8 col-md-6 col-sm-12" style="padding: 0;">
+      <img src="images/Visit Sinharaja Rain forest~~!!!.jfif" alt="Placeholder image" width="1200" height="900" class="img-fluid">
+    </div>
+  </div>
+  
+  </div>
+
+  <div class="container ">
+            
+            <!--ending of card-->
+            <h3 style="padding-top: 3%;padding-bottom: 2%;"><strong></strong> </h3>
+            <h2 >Best Rainforests in Sri Lanka</h2>
+            <p ><font size="4"></font>Enchanting rainforest ecosystems thrive here</p>
+            <!--beginnig of card-->
+
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/sin.jpg" class="img-fluid rounded-start" alt="..." >
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Sinharaja Rainforest</h5>
+                    <p class="card-text">Sinharaja Rainforest, a UNESCO World Heritage site in Sri Lanka, captivates with its biodiversity, lush foliage, and unique ecosystems.</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.kitulgalaadventures.com/" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-5" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/ka1.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Kanneliya Rainforest</h5>
+                    <p class="card-text">Kanneliya Rainforest, a tropical paradise in Sri Lanka, captivates with its rich biodiversity, lush greenery, and an array of exotic flora and fauna.</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://sigiriyafortress.com/" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>  
+          </div>
+            <!--end-->
+            <div class="container mt-5 mb-5">
+              <div class="row">
+                <div class="col-lg-4 col-md-6 col-sm-12" style="background-color: #D3D0D0; padding: 0;">
+                  <br> &nbsp;<center>
+                  <h4><b><font style="font-family: Poppins, sans-serif;" color="black">Waterfalls in Sri Lanka</font></b></h4></center><br>
+                  <p style="margin: 20px;"> <font style="font-size: 15px; ">
+                 Sri Lanka, known as the "Pearl of the Indian Ocean," boasts a mesmerizing array of waterfalls that cascade down lush green landscapes, captivating locals and tourists alike. These natural wonders, scattered across the island's diverse topography, offer breathtaking sights and soothing sounds that refresh the soul. The impressive Dunhinda Falls, with its 64-meter drop, is a highlight, while Bambarakanda, the country's tallest waterfall, towers at 263 meters. Visitors can trek through rainforests to witness the mystical beauty of Diyaluma Falls or admire the picturesque Rawana Falls, steeped in ancient legends. The captivating allure of Sri Lanka's waterfalls makes it a paradise for nature enthusiasts and photographers.</font></p>
+                </div>
+                <div class="col-lg-8 col-md-6 col-sm-12" style="padding: 0;">
+                  <img src="images/Diyaluma-Falls-2.jpg" alt="Placeholder image" width="900" height="600" class="img-fluid" style="object-fit: cover;">
+                </div>
+              </div>
+              <br><br>
+
+              <div class="container ">
+               
+
+            <h2 class="" style="padding-top: 4%;" >Best Waterfalls in Sri Lanka</h2>
+            <p ><font size="4">Sri Lanka's mesmerizing waterfalls enchant all</font></p>
+            <!--beginnig of card-->
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/bambarakanda-waterfall.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Bambarakanda Falls</h5>
+                    <p class="card-text">Bambarakanda Falls: Sri Lanka's tallest cascade, captivating with its enchanting beauty amidst lush hills</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/Dunhinda_Ella.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Dunhinda Falls</h5>
+                    <p class="card-text">A picturesque marvel in Sri Lanka, cascading gracefully amidst verdant landscapes, captivating all who visit</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/Bopath-ella.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Bopath Ella</h5>
+                    <p class="card-text">Enthralls with its unique boomerang shape and serene surroundings, an alluring natural wonder</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/Ravana-ella.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Ravana Falls</h5>
+                    <p class="card-text">Discover the captivating beauty of Ravana Falls, a mesmerizing waterfall nestled in pristine natural surroundings</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/Devon.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Devon Falls</h5>
+                    <p class="card-text">Nature's breathtaking masterpiece, an enchanting cascade of beauty nestled amid serene wilderness and tranquility</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/Diyaluma-Falls-1.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Diyaluma Falls</h5>
+                    <p class="card-text">Nature's majestic wonder, cascading waters, and breathtaking beauty in Sri Lanka's pristine wilderness</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/Laxapana-Falls.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Laxapana Falls</h5>
+                    <p class="card-text">A majestic Sri Lankan wonder, cascades down gracefully amidst lush green landscapes and serene beauty</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="card mb-3" onmouseover="changeCardColor(this)" onmouseout="resetCardColor(this)">
+              <div class="row g-0">
+                <div class="col-md-4">
+                  <img src="images/baker-s-fall-in--1.jpg" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                  <div class="card-body">
+                    <h5 class="card-title">Baker's Falls</h5>
+                    <p class="card-text">A picturesque cascading waterfall nestled amidst lush greenery, offering a serene natural retreat for visitors</p>
+                    <button type="button" class="btn btn-dark btn-rounded ms-2 btn1" ><a href="https://www.rapidadventures.lk/experiences/waterfall-abseiling-at-meemure" target="_blank">See more</a></button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+
+        <!--Footer----------------------------------------------------------Start-->
+    <footer class="footer">
+      <div class="container" style="text-align: center;">
+        <div class="row">
+          <div class="footer-col col-md-4 mt-3 ">
+            <ul style="list-style-type:none;">
+              <div class="footer-img">
+                <img src="images/walkers-footer.png" alt="" style="max-width: 200px;">
+              </div>
+            </ul>
+          </div>
+          <div class="footer-col col-md-2 mt-3 ">
+            <ul style="list-style-type:none;">
+              <h5 class="footer-topics">Menu</h5>
+              <li class="footer-listed-items"><a href="www.google.com">Home</a></li>
+              <li class="footer-listed-items"><a href="www.google.com">FAQ</a></li>
+              <li class="footer-listed-items"><a href="www.google.com">About Us</a></li>
+              <li class="footer-listed-items"><a href="www.google.com">Join Us</a></li>
+            </ul>
+          </div>
+          <div class="footer-col col-md-2 mt-3">
+            <ul style="list-style-type:none;">
+              <h5 class="footer-topics">Catogaries</h5>
+              <li class="footer-listed-items"><a href="www.google.com" target="_blank">Heritage</a></li>
+              <li class="footer-listed-items"><a href="www.google.com" target="_blank">Beaches</a></li>
+              <li class="footer-listed-items"><a href="www.google.com" target="_blank">Wild-Life</a></li>
+              <li class="footer-listed-items"><a href="www.google.com" target="_blank">Rainforests</a></li>
+              <li class="footer-listed-items"><a href="www.google.com" target="_blank">Adventure</a></li>
+              <li class="footer-listed-items"><a href="www.google.com" target="_blank">Accomedations</a></li>
+            </ul>
+          </div>
+          <div class="footer-col col-md-4 mt-3">
+            <h4>Contact Us</h4>
+            <p class="contact-footer">www.walkers.srilanka@gmail.com</p>
+            <button type="button" class="btn btn-dark btn-rounded ms-2 btn1"><a href="">Our Team</a></button>
+
+          </div>
+        </div>
+      </div>
+      <div style="height: 50px; max-width: 100%; background-color: #012e01 ;" class="copyright-div">
+        <p class="copyright">Copyright © 2023 Walkers Sri Lanka. All rights reserved</p>
+      </div>
+    </footer>
+    <!--Footer----------------------------------------------------------End-->
+
+
+       <script src="js/script-RW.js"></script>         
+</body>
+</html>
