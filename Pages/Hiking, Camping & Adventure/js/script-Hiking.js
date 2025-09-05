8b6398f9fbfb640a69e1243aa81f42ce46cdc1e1
Dinuvi Tharushika <135813670+Dinuvi9170@users.noreply.github.com>
2025-09-05 23:16:54 +0530

    Add files via upload

---
+++ Pages/Hiking, Camping & Adventure/js/script-Hiking.js
@@ -0,0 +1,9 @@
+function changeCardColor(card) {
+    card.style.backgroundColor = "#c6c6c6"; // Change the card color to blue on mouseover
+  }
+  
+  function resetCardColor(card) {
+    card.style.backgroundColor = " #ffffff "; // Reset the card color to the original color on mouseout
+  }
+
+  
